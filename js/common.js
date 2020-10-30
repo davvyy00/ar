da3708f4c25904df2c674917154e87a786000c4a
david.keane <david.keane@interdynamix.com>
2020-10-30 11:14:06 -0600

    make webpage display full screen

---
+++ js/common.js
@@ -0,0 +1,29 @@
+var Common = (function() {
+    console.log('IFFE called');
+    let makeFullScreen = () => {
+    	console.log('make full screen');
+    	window.scrollTo(0,1);
+    	
+        var doc = window.document;
+        var docEl = doc.documentElement;
+
+        var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+        var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+        if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+            requestFullScreen.call(docEl);
+        } else {
+            cancelFullScreen.call(doc);
+        }
+    }
+    return {
+        makeFullScreen
+    };
+})();
+
+Common.makeFullScreen();
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    //the event occurred
+    console.log('DOM loaded');
+});
