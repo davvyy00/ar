28b9efd82133052e97299365242f5cdbd7045e07
david.keane <david.keane@interdynamix.com>
2020-11-24 15:26:53 -0700

    try with obsucated code

--- buildProject.js
+++ buildProject.js
@@ -1,25 +1,40 @@
 var fs = require('fs');
 
-//get a reference to the uglify-js module
-var UglifyJS = require('uglify-es');
+var JavaScriptObfuscator = require('javascript-obfuscator');
 
-var appClientFiles = {
-    "services.js": fs.readFileSync('js/services.js', 'utf8'),
-    "common.js": fs.readFileSync('js/common.js', 'utf8')
-}
+var common = fs.readFileSync('js/common.js', 'utf8');
+var services = fs.readFileSync('js/services.js', 'utf8');
+
 
 var options = {
-    mangle: true
+	compact: true,
+	controlFlowFlattening: true,
+	controlFlowFlatteningThreshold: 1,
+	deadCodeInjection: true,
+	deadCodeInjectionThreshold: 1,
+	debugProtection: true,
+	debugProtectionInterval: true,
+	disableConsoleOutput: true,
+	log: false,
+	mangle: false,
+	renameGlobals: false,
+	rotateStringArray: true,
+	selfDefending: true,
+	stringArray: true,
+	stringArrayEncoding: ['rc4'],
+	stringArrayThreshold: 1,
+	unicodeEscapeSequence: false
 };
 
 //get a reference to the minified version of file-1.js, file-2.js and file-3.js
-var result = UglifyJS.minify(appClientFiles, options);
+var result = JavaScriptObfuscator.obfuscate(common, options);
+var result2 = JavaScriptObfuscator.obfuscate(services, options);
 
 //view the output
 //console.log(result);
-console.log(result.code);
+console.log(result.getObfuscatedCode());
 
-fs.writeFile("output.min.js", result.code, function(err) {
+fs.writeFile("output.min.js", result.getObfuscatedCode() + result2.getObfuscatedCode(), function(err) {
     if (err) {
         console.log(err);
     } else {
